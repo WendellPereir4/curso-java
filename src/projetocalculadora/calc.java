3b1aba3ee6f7ac4c2d0f87ec6d571a42a91a8254
wendell <wendellluciopereira4080@gmail.com>
2026-08-24 19:05:48 -0300

    colocandoOPERADORMENOS

--- src/projetocalculadora/calc.java
+++ src/projetocalculadora/calc.java
@@ -11,15 +11,21 @@ public class calc {
         System.out.print("Digite seu numero: ");
         int numerosoma1 = fc.nextInt();
 
+        // operador
         System.out.print("Digite o operador: ");
-        String operador = fc.next();
+        String mais = fc.next();
+        String menos = fc.next();
+
+
 
         System.out.print("Digite o numero para somar: ");
         int numerosoma2 = fc.nextInt();;
 
-
-       if (operador == "+");
+        if (mais == "+")
         System.out.println(numerosoma1 + numerosoma2);
+
+        else if (menos == "-");
+        System.out.println(numerosoma1-numerosoma2);
 
 
 
