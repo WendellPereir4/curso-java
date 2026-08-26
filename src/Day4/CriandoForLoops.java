b0a0c8a42bfcd90f1cdfc75fc4e9ea11e711432f
wendell <wendellluciopereira4080@gmail.com>
2026-08-26 20:59:05 -0300

    Adicionado conhecimentos com LoopsArrays, carro.length

---
+++ src/Day4/CriandoForLoops.java
@@ -0,0 +1,10 @@
+package Day4;
+
+public class CriandoForLoops {
+     public static void main(String[] args) {
+
+         for (int i = 1; i < 11; i++)
+            System.out.print(i);
+
+    }
+}
---
+++ src/Day4/LoopsArrays.java
@@ -0,0 +1,12 @@
+package Day4;
+
+public class LoopsArrays {
+public static void main(String[] args) {
+
+        String[] carro = {"BMW, TESLA, FIAT, ferrari, ferrari, ferrari, ferrari"};
+
+        for (int i = 0; i < carro.length; i++)
+            System.out.println(carro[i]);
+
+    }
+}
