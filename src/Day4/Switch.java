19771ba56da5714f24b70291043f8c6df729d508
wendell <wendellluciopereira4080@gmail.com>
2026-08-26 13:46:17 -0300

    adicioando lowerCase para entrada de strig maiscula

--- src/Day4/Switch.java
+++ src/Day4/Switch.java
@@ -8,12 +8,20 @@ public static void main(String[] args) {
         // coletar dado do produto
         System.out.print("Digite nome do produto: ");
         Scanner s = new Scanner(System.in);
-        String nomedoproduto = s.next();
+        String nomedoproduto = s.next().toLowerCase(); // -> se o usuário digitar maisculo, o sistema imprimi como minisculo.
         //retornar valor do produto
 
         switch (nomedoproduto) {
             case "banana":
                 System.out.println("R$9,50");
+                break;
+
+            case "manga":
+                System.out.println("R$12,80");
+                break;
+
+            case "abacate":
+                System.out.println("R$16,10");
                 break;
     }
 
