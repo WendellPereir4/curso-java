858ee190cb8f6a3f56f03150f500ed228cb8fe85
wendell <wendellluciopereira4080@gmail.com>
2026-08-27 00:00:54 -0300

     criando for loops

--- src/Day4/NumerosParesEXER.java
+++ src/Day4/NumerosParesEXER.java
@@ -1,4 +1,11 @@
 package Day4;
 
 public class NumerosParesEXER {
+public static void main(String[] args) {
+
+
+    for (int i = 0; i <= 50; i++)
+
+        System.out.print(i*2 + " ");
+    }
 }
--- src/Day4/WhileLOOPING.java
+++ src/Day4/WhileLOOPING.java
@@ -1,4 +1,13 @@
 package Day4;
 
 public class WhileLOOPING {
+public static void main(String[] args) {
+
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i + " - Hi");
+            i++;
+        }
+
+    }
 }
