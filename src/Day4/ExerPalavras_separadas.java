e60fea6be73bde9c186ce38b4618a64919592508
wendell <wendellluciopereira4080@gmail.com>
2026-08-27 17:38:15 -0300

    criando exercicio com lenght e space

---
+++ src/Day4/ExerPalavras_separadas.java
@@ -0,0 +1,12 @@
+package Day4;
+
+public class ExerPalavras_separadas {
+    static void main(String[] args) {
+
+        for (int i = 1; i < 4; i++)
+            System.out.print("H e l l o" + "--");
+
+        // space e lengt -> mt util
+
+    }
+}
